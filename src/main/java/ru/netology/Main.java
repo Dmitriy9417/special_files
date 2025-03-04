80105dbedda1481696d7d143c29a5baeaf18b641
Dimi <rub.dimas@yandex.ru>
2025-03-04 20:33:25 +0300

    task1 completed

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,7 +7,6 @@ import com.opencsv.exceptions.CsvValidationException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
